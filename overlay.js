f85ded9023fbf73e3598b150d37f67348843a7e6
lu2000luk <81090607+lu2000luk@users.noreply.github.com>
2023-04-09 21:03:58 +0200

    Update overlay.js

--- overlay.js
+++ overlay.js
@@ -1,4 +1,4 @@
-function showOverlay(message) {
+function showOverlay(message, duration) {
     var overlay = $('<div class="overlay"></div>');
     var message = $('<div class="overlayMessage">'+message+'</div>');
     overlay.append(message);
@@ -8,7 +8,7 @@ function showOverlay(message) {
       overlay.fadeOut(function() {
         $(this).remove();
       });
-    }, 800);
+    }, duration);
   }
 
 document.head.innerHTML += "<style>.overlay {position: fixed;top: 0;left: 0;width: 100%; height: 100%;background-color: rgba(0,0,0,0.6);z-index: 9999;display: none;}.overlayMessage {font-size: 60px;font-weight: bold;color: white;position: absolute;top: 50%;left: 50%;transform: translate(-50%, -50%);font-family: 'Seoge UI', sans-serif;}</style>";
